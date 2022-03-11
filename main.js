e9a44ebaaa6a26922a803f1af1c56d0f2079ba09
vexe141 <sadbhavadhikari@gmail.com>
2022-03-11 21:38:08 +0545

    code formatting improved

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ let spacesVal =
         s31: document.getElementById('31'),
         s32: document.getElementById('32'),
         s33: document.getElementById('33')
-}
+};
 
-let clickArea = document.getElementById('totalSpaces')
-let symb = 'O'
-let result = document.getElementById('result')
+let clickArea = document.getElementById('totalSpaces');
+let symb = 'O';
+let result = document.getElementById('result');
 
 function testMatch()
 {
@@ -26,12 +26,12 @@ function testMatch()
         || spacesVal.s21.innerHTML !== '...' && spacesVal.s22.innerHTML !== '...' && spacesVal.s23.innerHTML !== '...' && spacesVal.s21.innerHTML === spacesVal.s22.innerHTML && spacesVal.s22.innerHTML === spacesVal.s23.innerHTML 
         || spacesVal.s31.innerHTML !== '...' && spacesVal.s32.innerHTML !== '...' && spacesVal.s33.innerHTML !== '...' && spacesVal.s31.innerHTML === spacesVal.s32.innerHTML && spacesVal.s32.innerHTML === spacesVal.s33.innerHTML)
     {
-        result.innerHTML = `${symb} player wins!`
+        result.innerHTML = `${symb} player wins!`;
     }
 
     else
     {
-        return
+        return;
     }
 }
 
@@ -39,69 +39,74 @@ function changeSymb()
 {
     if(symb == 'O')
     {
-        symb = 'X'
+        symb = 'X';
     }
+
     else
     {
-        symb = 'O'
+        symb = 'O';
     }
 }
 
 spacesVal.s11.addEventListener('click', () =>
 {
-        spacesVal.s11.innerHTML = symb
-        testMatch()
-        changeSymb()
+        spacesVal.s11.innerHTML = symb;
+        testMatch();
+        changeSymb();
 })
+
 spacesVal.s12.addEventListener('click', () =>
 {
-        spacesVal.s12.innerHTML = symb
-        testMatch()
-        changeSymb()
+        spacesVal.s12.innerHTML = symb;
+        testMatch();
+        changeSymb();
 })
+
 spacesVal.s13.addEventListener('click', () =>
 {
-        spacesVal.s13.innerHTML = symb
-        testMatch()
-        changeSymb()
+        spacesVal.s13.innerHTML = symb;
+        testMatch();
+        changeSymb();
 })
+
 spacesVal.s21.addEventListener('click', () =>
 {
-        spacesVal.s21.innerHTML = symb
-        testMatch()
-        changeSymb()
+        spacesVal.s21.innerHTML = symb;
+        testMatch();
+        changeSymb();
 })
+
 spacesVal.s22.addEventListener('click', () =>
 {
-        spacesVal.s22.innerHTML = symb
-        testMatch()
-        changeSymb()
+        spacesVal.s22.innerHTML = symb;
+        testMatch();
+        changeSymb();
 })
+
 spacesVal.s23.addEventListener('click', () =>
 {
-        spacesVal.s23.innerHTML = symb
-        testMatch()
-        changeSymb()
+        spacesVal.s23.innerHTML = symb;
+        testMatch();
+        changeSymb();
 })
+
 spacesVal.s31.addEventListener('click', () =>
 {
-        spacesVal.s31.innerHTML = symb
-        testMatch()
-        changeSymb()
+        spacesVal.s31.innerHTML = symb;
+        testMatch();
+        changeSymb();
 })
+
 spacesVal.s32.addEventListener('click', () =>
 {
-        spacesVal.s32.innerHTML = symb
-        testMatch()
-        changeSymb()
+        spacesVal.s32.innerHTML = symb;
+        testMatch();
+        changeSymb();
 })
+
 spacesVal.s33.addEventListener('click', () =>
 {
-        spacesVal.s33.innerHTML = symb
-        testMatch()
-        changeSymb()
+        spacesVal.s33.innerHTML = symb;
+        testMatch();
+        changeSymb();
 })
-
-
-    
-
